1677616853657fc97f0d3edfd99f027d9bce1491
Dan Albors Giner <43437599+DanMkAg@users.noreply.github.com>
2023-09-11 12:17:02 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,33 +1,33 @@
 #ChatBot inteligente con WhatsApp en Python
 from flask import Flask, jsonify, request
 app = Flask(__name__)
-#CUANDO RECIBAMOS LAS PETICIONES EN ESTA RUTA
+#CUANDO RECIBA LAS PETICIONES EN ESTA RUTA
 @app.route("/webhook/", methods=["POST", "GET"])
 def webhook_whatsapp():
     #SI HAY DATOS RECIBIDOS VIA GET
     if request.method == "GET":
-        #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
+        #SI EL TOKEN ES IGUAL AL QUE RECIBO
         if request.args.get('hub.verify_token') == "QigongValencia":
-            #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
+            #ESCRIBO EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
             return request.args.get('hub.challenge')
         else:
-            #SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
+            #SI NO SON IGUALES SE DEVUELVE UN MENSAJE DE ERROR
           return "Error de autentificacion."
-    #RECIBIMOS TODOS LOS DATOS ENVIADO VIA JSON
+    #RECIBO TODOS LOS DATOS ENVIADOS VIA JSON
     data=request.get_json()
-    #EXTRAEMOS EL NUMERO DE TELEFONO Y EL MANSAJE
+    #EXTRAIGO EL NUMERO DE TELEFONO Y EL MANSAJE
     telefonoCliente=data['entry'][0]['changes'][0]['value']['messages'][0]['from']
-    #EXTRAEMOS EL TELEFONO DEL CLIENTE
+    #EXTRAIGO EL TELEFONO DEL CLIENTE
     mensaje=data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
-    #EXTRAEMOS EL ID DE WHATSAPP DEL ARRAY
+    #EXTRAIGO EL ID DE WHATSAPP DEL ARRAY
     idWA=data['entry'][0]['changes'][0]['value']['messages'][0]['id']
-    #EXTRAEMOS EL TIEMPO DE WHATSAPP DEL ARRAY
+    #EXTRAIGO EL TIEMPO DE WHATSAPP DEL ARRAY
     timestamp=data['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']
-    #ESCRIBIMOS EL NUMERO DE TELEFONO Y EL MENSAJE EN EL ARCHIVO TEXTO
+    #ESCRIBO EL NUMERO DE TELEFONO Y EL MENSAJE EN EL ARCHIVO TEXTO
     #SI HAY UN MENSAJE
     if mensaje is not None:
       from rivescript import RiveScript
-      #INICIALIZAMOS RIVESCRIPT Y CAMBIO CONVERSACION
+      #INICIALIZO RIVESCRIPT Y CAMBIO CONVERSACION
       bot = RiveScript()
       bot.load_file('chatbot.rive')
       bot.sort_replies()
@@ -35,11 +35,39 @@ def webhook_whatsapp():
       respuesta = bot.reply("localuser", mensaje)
       respuesta = respuesta.replace("\\n","\\\n")
       respuesta = respuesta.replace("\\","")
-      f = open("texto.txt", "w")
-      f.write(respuesta)
-      f.close()
-      #RETORNAMOS EL STATUS EN UN JSON
+       #CONECTO A LA BASE DE DATOS
+      import mysql.connector
+      mydb = mysql.connector.connect(
+          host = "DIRECCION HOST",
+          user = "USUARIO BASE DE DATOS",
+          password = "CONTRASEÑA BASE DE DATOS",
+          database='NOMBRE BASE DE DATOS')
+      mycursor = mydb.cursor()
+      query="SELECT count(id) AS cantidad FROM registro WHERE id_wa='" + idWA + "';"
+      mycursor.execute("SELECT count(id) AS cantidad FROM registro WHERE id_wa='" + idWA + "';")
+      cantidad, = mycursor.fetchone()
+      cantidad=str(cantidad)
+      cantidad=int(cantidad)
+      if cantidad==0 :
+        sql = ("INSERT INTO registro"+ 
+        "(mensaje_recibido,mensaje_enviado,id_wa      ,timestamp_wa   ,telefono_wa) VALUES "+
+        "('"+mensaje+"'   ,'"+respuesta+"','"+idWA+"' ,'"+timestamp+"','"+telefonoCliente+"');")
+        mycursor.execute(sql)
+        mydb.commit()
+        enviar(telefonoCliente,respuesta)
+      #RETORNO EL STATUS EN UN JSON
       return jsonify({"status": "success"}, 200)
-#INICIAMSO FLASK
+def enviar(telefonoRecibe,respuesta):
+  from heyoo import WhatsApp
+  #TOKEN DE ACCESO DE FACEBOOK
+  token='EAADFmKPVdXgBO3JzqBeJykeKMUldZCW6s9P07rOR0dQTOu3b1xKzVSUJpZC7mVCkJsszuq69twlehUYYVeMwjY8l4JLZArAZCNWqL7ZA9i2cMuelEs400ufCHWwhn9Kz2kFMzeQvkesXzGdgIZA0ZBB7hTdg1o6SX5vkZAqmucD3opedLHVivBdagYbZAReUbGCFs8wvWZAqb2l9uLdFgWaZAsLg24R2WSb'
+  #IDENTIFICADOR DE NÚMERO DE TELÉFONO
+  idNumeroTeléfono='138300759355557'
+  #INICIALIZO ENVIO DE MENSAJES
+  mensajeWa=WhatsApp(token,idNumeroTeléfono)
+  telefonoRecibe=telefonoRecibe.replace("521","52")
+  #ENVIO UN MENSAJE DE TEXTO
+  mensajeWa.send_message(respuesta,telefonoRecibe)
+#INICIO FLASK
 if __name__ == "__main__":
   app.run(debug=True)
