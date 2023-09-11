79157f5f49272626b358d24813ba75cc8865f5f0
Dan Albors Giner <43437599+DanMkAg@users.noreply.github.com>
2023-09-11 12:11:15 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,45 @@
+#ChatBot inteligente con WhatsApp en Python
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+#CUANDO RECIBAMOS LAS PETICIONES EN ESTA RUTA
+@app.route("/webhook/", methods=["POST", "GET"])
+def webhook_whatsapp():
+    #SI HAY DATOS RECIBIDOS VIA GET
+    if request.method == "GET":
+        #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
+        if request.args.get('hub.verify_token') == "QigongValencia":
+            #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
+            return request.args.get('hub.challenge')
+        else:
+            #SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
+          return "Error de autentificacion."
+    #RECIBIMOS TODOS LOS DATOS ENVIADO VIA JSON
+    data=request.get_json()
+    #EXTRAEMOS EL NUMERO DE TELEFONO Y EL MANSAJE
+    telefonoCliente=data['entry'][0]['changes'][0]['value']['messages'][0]['from']
+    #EXTRAEMOS EL TELEFONO DEL CLIENTE
+    mensaje=data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+    #EXTRAEMOS EL ID DE WHATSAPP DEL ARRAY
+    idWA=data['entry'][0]['changes'][0]['value']['messages'][0]['id']
+    #EXTRAEMOS EL TIEMPO DE WHATSAPP DEL ARRAY
+    timestamp=data['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']
+    #ESCRIBIMOS EL NUMERO DE TELEFONO Y EL MENSAJE EN EL ARCHIVO TEXTO
+    #SI HAY UN MENSAJE
+    if mensaje is not None:
+      from rivescript import RiveScript
+      #INICIALIZAMOS RIVESCRIPT Y CAMBIO CONVERSACION
+      bot = RiveScript()
+      bot.load_file('chatbot.rive')
+      bot.sort_replies()
+      #OBTENGO LA RESPUESTA
+      respuesta = bot.reply("localuser", mensaje)
+      respuesta = respuesta.replace("\\n","\\\n")
+      respuesta = respuesta.replace("\\","")
+      f = open("texto.txt", "w")
+      f.write(respuesta)
+      f.close()
+      #RETORNAMOS EL STATUS EN UN JSON
+      return jsonify({"status": "success"}, 200)
+#INICIAMSO FLASK
+if __name__ == "__main__":
+  app.run(debug=True)
